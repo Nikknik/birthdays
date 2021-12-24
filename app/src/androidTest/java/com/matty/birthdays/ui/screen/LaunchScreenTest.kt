7da943570aaa1d731e6b45372b48703d5ea5d6a0
Aleksandr <asrbbnk@gmail.com>
2021-12-24 14:27:16 +0300

    Add launch screen

---
+++ app/src/androidTest/java/com/matty/birthdays/ui/screen/LaunchScreenTest.kt
@@ -0,0 +1,43 @@
+package com.matty.birthdays.ui.screen
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class LaunchScreenTest {
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun shouldCallHandlerExactlyOnceWhenDelayIsOver() {
+        var handlerInvocationTimes = 0
+        val onDelayFinished = { handlerInvocationTimes += 1 }
+
+        composeRule.setContent {
+            LaunchScreen(delayMillis = 0, onDelayFinished)
+        }
+
+        assertEquals(1, handlerInvocationTimes)
+    }
+
+    @Test
+    fun shouldShowAppName() {
+        composeRule.setContent {
+            LaunchScreen(delayMillis = 0) {}
+        }
+
+        composeRule.onNodeWithTag("appName").assertIsDisplayed()
+    }
+
+    @Test
+    fun shouldShowLogo() {
+        composeRule.setContent {
+            LaunchScreen(delayMillis = 0) {}
+        }
+
+        composeRule.onNodeWithTag("image").assertIsDisplayed()
+    }
+}
